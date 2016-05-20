735f4ba94af52dc038844a4dc589233c397795c1
郭金鑫 <princergx@live.com>
2016-05-20 15:05:25 +0800

    初始化

---
+++ 源.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+void abc();
+
+int main()
+{
+	abc();
+	abc();
+	return 0;
+}
+
+void abc()
+{
+	int r = 0, y, n, p;
+	cout << "最大日期2015年8月请输入年份：";
+	cin >> n;
+	cout << endl;
+	cout << "请输入月份：";
+	cin >> y;
+	cout << endl;
+	cout << "请输入批次：";
+	cin >> p;
+	for (size_t i = 0; i < 31; i++)
+	{
+		r = r + 1;
+		cout << "http://down.jyfz.info/mnt/" << n << "-" << y << "-" << r << "-" << p << ".rar" << endl;
+	}
+}
